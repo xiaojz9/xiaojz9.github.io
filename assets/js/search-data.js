d5601b1640d58b4fedf3434ee470b92774b7eaca
xiaojz9 <165420353+xiaojz9@users.noreply.github.com>
2026-02-26 09:46:00 +0000

    Deploying to gh-pages from @ xiaojz9/xiaojz9.github.io@f48394c33921adc430cdf74bed3cd73f5d5183fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -117,17 +117,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/echarts/";
           
         },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/chartjs/";
-          
-        },
       },{id: "post-a-post-with-bibliography",
         
           title: "a post with bibliography",
