4b5728bcd1243906f575d297f9477f5f56c9893c
xiaojz9 <165420353+xiaojz9@users.noreply.github.com>
2026-03-07 01:01:21 +0000

    Deploying to gh-pages from @ xiaojz9/xiaojz9.github.io@a150c27bd5af45e35d532f43f983193a25c09a7a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -127,16 +127,6 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
